2df3219ce823dd23a28cc9ec9dce21b75ea9c8e2
Wierdtangaming <33354033+Wierdtangaming@users.noreply.github.com>
2019-12-17 15:12:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,51 +1,51 @@
 
 var r = 0;
 var g = 50;
-var b=255;
-
-var randXY = 0;
-
-var count = 0;
-
-var ellipse,ellipse_img;
-
-// INITIALIZE ALL YOUR VARIABLES
-// Ideally you would create 3 variables. red blue green or r, g, b
-
-
-
-function preload() {
-
-  ellipse_img("ellipse.png");
-
-
-}
-
+var b=255
+var rand,count;
+var X,Y;
 
 
 function setup(){
-  ellipse = createSprite(200,200,50,50);
-  ellipse.addImage("glorified circle", ellipse_img);
   createCanvas(768,128);
+  rand = 0;
+  count = 0;
 }
 
-function draw(){
+
+function draw() {
+if (count % 60 === 0) {
+  X = World.mouseX
+  Y = World.mouseY
+}
+if (X === World.mouseX && Y === World.mouseY) {
+  if (count % 4 === 0){
+    if (rand === 0) {
+    update_1();
+    }
+    else if (rand === 1) {
+      update_2();
+    }
+  }
+}
+else {
+update_1();
+} 
   count++;
-  randXY = Math.round(random(0,1));
-  // change the value of Red based on the mouse movemeadwnt about the X axis
-    r = World.mouseX / 3;
-    g = World.mouseY * 2;
-    b = World.mouseX / 3;
-
-  // change the value of Green based on the mouse movement about the X axis
-  // change the value of Blue based on the mouse movement about the X axis
-
-  // Use the map() function to do so. 
-
-  // Pass the values to the background() function you have learnt previously.
+  rand = Math.round(random(0,1));
   background(r,g,b);
+}
 
-  // Create an ellipse that will move around with you mouse about the X axis.
 
-  // Remember to fill the canvas with white paint before creating the ellipse.
+function update_1() { 
+  r = World.mouseX / 3;
+  g = World.mouseY * 2;
+  b = World.mouseX / 3;
+
+}
+function update_2() { 
+  r = World.mouseX * 2;
+  g = World.mouseY / 3;
+  b = World.mouseX * 2;
+
 }
